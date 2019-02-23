485bbbd8465f2f87eac16a4843048bcb0dfaecb2
kataya1 <34242491+kataya1@users.noreply.github.com>
2019-02-23 16:17:31 +0200

    added process time calculation to check effitency and debugger and updated python version 3.0

--- febonachi.py
+++ febonachi.py
@@ -1,3 +1,6 @@
+import time
+import pdb
+# breakpoint()
 def fib(n):
     n_old = 0
     result = 1
@@ -7,14 +10,15 @@ def fib(n):
         n_old = n_new
     return result
 
-
+start1 = time.perf_counter_ns()
 def printfib(n):
     for i in range(n):
         print(i+1, end=" ")
         # print("_",end="")
         print(fib(i))
-
+end1 = time.perf_counter_ns()
 #efficent fib
+start2 = time.perf_counter_ns()
 def print_fib_efficent(n):
     n_old = 0
     result = 1
@@ -23,9 +27,15 @@ def print_fib_efficent(n):
         n_new = result
         result = n_new + n_old
         n_old = n_new
+end2 = time.process_time_ns()
+printfib(1000)
+print_fib_efficent(10000)
+print(end1 - start1)
+print(end2 - start2)
+def fib_memoize(n):
+    memo = [None] * (n+1)
 
-printfib(9)
-print_fib_efficent(9)
-# # efficent implementation of fib
+# object oriented implementation of fib
 # class fib(n):
 #
+
